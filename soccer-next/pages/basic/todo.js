2fb0d0642dc58c5e8a98626d5397fa1cd29e75d0
meanseo <chlalstjok97@gmail.com>
2022-03-28 17:38:34 +0900

    2022-03-28 17:38 todo추가

---
+++ soccer-next/pages/basic/todo.js
@@ -0,0 +1,8 @@
+export default function Todo(){
+    return (
+        <div className="Todo">
+        <input type='checkbox' id="todo0" name="todo0" value="todo0" />
+        <label htmlFor="todo0">To-do</label>
+        </div>
+    )
+}
